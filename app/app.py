f5f6d08b904670aa683ee335d69253218067ed53
flightman69 <flightman69420@gmail.com>
2025-01-26 11:46:34 +0530

    updated app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "SpeakX Flask API (HTTPS Secured!)"
-
+@app.route('/status')
+def status():
+    jsonify({
+        "status": "API is up and Running"
+    })
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=port)
