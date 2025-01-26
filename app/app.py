daef6f69679ddd3eee2c3176befa2ec0619ca6cb
flightman69 <flightman69420@gmail.com>
2025-01-26 12:17:10 +0530

    docker compose

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "SpeakX Flask API (HTTPS Secured!)"
+    return "SpeakX Flask API (HTTPS Secured!)."
 @app.route('/status')
 def status():
     return jsonify({
