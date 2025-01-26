9e495c5977c5474156f08fda30ca5604be6fedd8
flightman69 <flightman69420@gmail.com>
2025-01-26 12:29:51 +0530

    added one more api

--- app/app.py
+++ app/app.py
@@ -11,6 +11,10 @@ def status():
     return jsonify({
         "status": "API is up and Running"
     })
+@app.route('/pic')
+def show_pic():
+    return send_from_directory('images', 'img.png')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=port)
